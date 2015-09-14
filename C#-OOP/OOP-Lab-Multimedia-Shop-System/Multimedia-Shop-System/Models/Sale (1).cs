ffbb846a3979f881422399e050c682afb4d9e585
Rumen Tsonkov <r.conkov@gmail.com>
2015-09-14 12:36:56 +0300

    Added more homeworks and materials.

---
+++ C#-OOP/OOP-Lab-Multimedia-Shop-System/Multimedia-Shop-System/Models/Sale (1).cs
@@ -0,0 +1,22 @@
+﻿namespace MultimediaShopSystem.Models
+{
+    using System;
+    using MultimediaShopSystem.Contracts;
+
+    public class Sale : Trade, ISale
+    {
+        public Sale(IItem item, DateTime saleDate)
+        {
+            this.Item = item;
+            this.SaleDate = saleDate;
+        }
+
+        public Sale(IItem item)
+        {
+            this.Item = item;
+            this.SaleDate = DateTime.Now;
+        }
+
+        public DateTime SaleDate { get; set; }
+    }
+}
